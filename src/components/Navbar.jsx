d28498c999fd3d1c7607eb0484ef738ca738a888
M DAYYAN <dayyanmohammed6@gmail.com>
2024-08-13 22:42:27 +0530

    Designing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,9 @@ const Navbar = () => {
   return (
     <nav className='flex items-center text-white h-16 justify-between px-3 fixed top-0 w-full z-20'>
       <h1 className='text-2xl font-semibold'>
+        <a href="/">
         <span className='text-gold'>&lt;</span>Pass<span className='text-gold'>Keeper/&gt;</span>
+        </a>
       </h1>
       <a href="https://www.instagram.com/mddayyan007/" target='_blank' rel="noopener noreferrer">
         <button className='bg-gold hover:bg-gold-dark p-2 border border-transparent rounded-full flex items-center text-black transition duration-300'>
